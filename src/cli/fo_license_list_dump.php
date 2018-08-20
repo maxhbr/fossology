67abb970b6096bc0dde38f7d7b928103d98ba52d
Maximilian Huber <maximilian.huber@tngtech.com>
2018-08-20 14:51:26 +0200

    feat(dump): dump to mongo

--- src/cli/fo_license_list_dump.php
+++ src/cli/fo_license_list_dump.php
@@ -34,11 +34,14 @@ require_once("$MODDIR/lib/php/common-cli.php");
 cli_Init();
 
 $Usage = "Usage: " . basename($argv[0]) . "
-  -u upload id        :: upload id
-  -t uploadtree id    :: uploadtree id
-  -c sysconfdir       :: Specify the directory for the system configuration
-  --username username :: username
-  --password password :: password
+  -u upload id             :: upload id
+  -t uploadtree id         :: uploadtree id
+  -c sysconfdir            :: Specify the directory for the system configuration
+  --username username      :: username
+  --password password      :: password
+  --mongohost host         :: host for mongo
+  --mongousername username :: username for mongo db
+  --mongopassword password :: password for mongo db
   -h  help, this message
 ";
 $upload = ""; // upload id
@@ -46,7 +49,7 @@ $item = ""; // uploadtree id
 
 $writeToStdout = true;
 
-$longopts = array("username:", "password:", "container:");
+$longopts = array("username:", "password:", "container:", "mongohost:", "mongousername:", "mongopassword:");
 $options = getopt("c:u:t:hxX:", $longopts);
 if (empty($options) || !is_array($options))
 {
@@ -55,6 +58,7 @@ if (empty($options) || !is_array($options))
 }
 
 $user = $passwd = "";
+$mongohost = $mongouser = $mongopasswd = "";
 foreach($options as $option => $value)
 {
   switch($option)
@@ -76,73 +80,39 @@ foreach($options as $option => $value)
     case 'password':
       $passwd = $value;
       break;
+    case "mongohost":
+      $mongohost = $value;
+      break;
+    case "mongousername":
+      $mongouser = $value;
+      break;
+    case "mongopassword":
+      $mongopasswd = $value;
+      break;
     default:
       print "unknown option $option\n";
       print $Usage;
   }
 }
 
-function __outputCSV(&$vals, $key, $filehandler) {
-    fputcsv($filehandler, $vals, ';', '"');
+if($user == "" ||
+   $passwd == "" ||
+   $mongohost == "" ||
+   $mongouser == "" ||
+   $mongopasswd == ""){
+  error_log("missing command line argument(s)");
+  return 1;
 }
 
-/*
- * based on LicenseDao::getAgentFileLicenseMatches
- */
-function getAgentFileLicenseMatchesForDump(DbManager $dbManager, ItemTreeBounds $itemTreeBounds)
-{
-    $uploadTreeTableName = $itemTreeBounds->getUploadTreeTableName();
-    $statementName = __METHOD__ . ".$uploadTreeTableName";
-    $uploadId = $itemTreeBounds->getUploadId();
-    $params = array($uploadId, $itemTreeBounds->getLeft(), $itemTreeBounds->getRight());
+function __outputToMongo(&$entry, $key, $bulk) {
+  $bulk->insert($entry);
+}
 
-    // TODO: remove unnecessary stuff!
-    $dbManager->prepare($statementName,
-        "(SELECT LFR.rf_shortname AS license_shortname,
-                LFR.rf_fullname AS license_fullname,
-                LFR.rf_pk AS license_id,
-                LFR.fl_pk AS license_file_id,
-                LFR.pfile_fk as file_id,
-                LFR.rf_match_pct AS percent_match,
-                AG.agent_name AS agent_name,
-                AG.agent_pk AS agent_id,
-                AG.agent_rev AS agent_revision,
-                PF.pfile_md5 AS pfile_md5,
-                PF.pfile_sha1 AS pfile_sha1,
-                PF.pfile_size AS pfile_size
-          FROM ( SELECT mlr.rf_fullname, mlr.rf_shortname, mlr.rf_pk, license_file.fl_pk, license_file.agent_fk, license_file.pfile_fk, license_file.rf_match_pct
-               FROM license_file JOIN ONLY license_ref mlr ON license_file.rf_fk = mlr.rf_pk) as LFR
-          INNER JOIN $uploadTreeTableName as UT ON UT.pfile_fk = LFR.pfile_fk
-          INNER JOIN agent as AG ON AG.agent_pk = LFR.agent_fk
-          INNER JOIN pfile as PF ON LFR.pfile_fk = pfile_pk
-          WHERE AG.agent_enabled='true' and
-           UT.upload_fk=$1 AND UT.lft BETWEEN $2 and $3
-          )
-          ");
-    $result = $dbManager->execute($statementName, $params);
-
-
-    $matches = array();
-    while ($row = $dbManager->fetchArray($result))
-    {
-        $fileNameInRepo=strtolower($row['pfile_sha1'].".".$row['pfile_md5']).".".$row['pfile_size'];
-        $fileNameInRepo= substr ($fileNameInRepo , 0, 2).'/'.
-            substr ($fileNameInRepo , 2, 2).'/'.
-            substr ($fileNameInRepo , 4, 2).'/'.$fileNameInRepo;
-
-        $matchData = array();
-        $matchData[] = $fileNameInRepo;
-        $matchData[] = '';
-        $matchData[] = $row['license_shortname'];
-        $matchData[] = $row['agent_name'];
-        $matchData[] = $row['file_id'];
-        $matchData[] = $uploadId;
-
-        $matches[] = $matchData;
-    }
-
-    $dbManager->freeResult($result);
-    return $matches;
+function writeDataToMongo(&$data, $manager){
+  $bulk = new MongoDB\Driver\BulkWrite;
+  array_walk($data, '__outputToMongo', $bulk);
+  $result = $manager->executeBulkWrite('rigel.file_raw', $bulk);
+  printf("Inserted %d documents\n", $result->getInsertedCount());
 }
 
 /*
@@ -160,15 +130,6 @@ function getClearedLicensesForDump(DbManager $dbManager, ItemTreeBounds $itemTre
     $condition = "ut.lft BETWEEN $1 AND $2";
 
     $groupId = Auth::getGroupId();
-
-//    // TODO: calls private method
-//    // $decisionsCte = $clearingDao->getRelevantDecisionsCte($itemTreeBounds, $groupId, $onlyCurrent=true, $statementName, $params, $condition);
-//    $reflector = new ReflectionObject($clearingDao);
-//    $method = $reflector->getMethod('getRelevantDecisionsCte');
-//    $method->setAccessible(true);
-//    $decisionsCte = $method->invokeArgs($clearingDao,
-//        array($itemTreeBounds, $groupId, $onlyCurrent=true, &$statementName, &$params, $condition));
-
 
     $uploadTreeTable = $itemTreeBounds->getUploadTreeTableName();
     $sql_upload = "";
@@ -209,7 +170,6 @@ function getClearedLicensesForDump(DbManager $dbManager, ItemTreeBounds $itemTre
               INNER JOIN license_ref lr ON lr.rf_pk = ce.rf_fk
               INNER JOIN pfile as PF ON decision.pfile_id = pfile_pk
             WHERE NOT ce.removed";
-    echo $sql;
 
     $dbManager->prepare($statementName, $sql);
 
@@ -222,15 +182,11 @@ function getClearedLicensesForDump(DbManager $dbManager, ItemTreeBounds $itemTre
             substr ($fileNameInRepo , 2, 2).'/'.
             substr ($fileNameInRepo , 4, 2).'/'.$fileNameInRepo;
 
-        $matchData = array();
-        $matchData[] = $fileNameInRepo;
-        $matchData[] = $row['shortname'];
-        $matchData[] = '';
-        $matchData[] = 'CONCLUSION';
-        $matchData[] = $row['file_id'];
-        $matchData[] = $uploadId;
-
-        $matches[] = $matchData;
+        $matches[] =[
+          'source' => 'CONCLUSION',
+          'path' => $fileNameInRepo,
+          'licenses' => [$row['shortname']]
+        ];
     }
 
     $dbManager->freeResult($result);
@@ -243,7 +199,6 @@ function getClearedLicensesForDump(DbManager $dbManager, ItemTreeBounds $itemTre
 function getBulkHistoryForDump($uploadTreeTableName, DbManager $dbManager)
 {
     $stmt = __METHOD__ . "." . $uploadTreeTableName . time();
-
     $sql = "WITH alltried AS (
             SELECT lr.lrb_pk, ce.clearing_event_pk ce_pk, lr.rf_text, ce.uploadtree_fk AS tried
             FROM license_ref_bulk lr
@@ -260,10 +215,8 @@ function getBulkHistoryForDump($uploadTreeTableName, DbManager $dbManager)
             FROM aggregated_tried
               INNER JOIN license_set_bulk lsb ON lsb.lrb_fk = lrb_pk
               INNER JOIN license_ref lrf ON lsb.rf_fk = lrf.rf_pk";
-
     $dbManager->prepare($stmt, $sql);
     $res = $dbManager->execute($stmt, array());
-
     $bulks = array();
     while ($row = $dbManager->fetchArray($res))
     {
@@ -280,99 +233,39 @@ function getBulkHistoryForDump($uploadTreeTableName, DbManager $dbManager)
         $key = $dbManager->booleanFromDb($row['removing']) ? 'removedLicenses' : 'addedLicenses';
         $bulks[$bulkRun][$key][] = $row['rf_shortname'];
     }
-
     $dbManager->freeResult($res);
     return $bulks;
 }
 
-function getItemTreeBounds($uploadtree_pk, $upload_pk)
+function dumpBulkList($uploadTreeTableName, $mongoManager)
 {
-    /* @var $uploadDao UploadDao */
-    $uploadDao = $GLOBALS['container']->get("dao.upload");
-
-    $uploadtreeTablename = GetUploadtreeTableName($upload_pk);
-    /** @var ItemTreeBounds */
-    return $uploadDao->getItemTreeBounds($uploadtree_pk, $uploadtreeTablename);
-}
-
-function getLicenseList($uploadtree_pk, $upload_pk, DbManager $dbManager)
-{
-
-    /** @var ItemTreeBounds */
-    $itemTreeBounds = getItemTreeBounds($uploadtree_pk, $upload_pk);
-
-  $outstream = null;
-  if(false)
-  {
-      $outstream = fopen("php://output", 'w');
-  }
-  else
-  {
-      $filename = "./dump-$upload_pk-$uploadtree_pk.csv";
-      error_log("... write to file=[$filename] into CWD");
-      $outstream = fopen($filename, "w");
-  }
-
-  if(false){ // skp scanner findings
-      $scannerLicenses = getAgentFileLicenseMatchesForDump($dbManager, $itemTreeBounds);
-      array_walk($scannerLicenses, '__outputCSV', $outstream);
-  }
-
-  if(true){ // dump conclusions
-      $clearedLicenses = getClearedLicensesForDump($dbManager, $itemTreeBounds);
-      array_walk($clearedLicenses, '__outputCSV', $outstream);
-  }
-  fclose($outstream);
-}
-
-function dumpBulkList($uploadTreeTableName)
-{
-    $bulkDir="./bulkFiles";
-    mkdir($bulkDir);
-
     /* @var $dbManager DbManager */
     $dbManager = $GLOBALS['container']->get('db.manager');
     $bulks = getBulkHistoryForDump($uploadTreeTableName, $dbManager);
 
-    $csvFile = "./bulkFiles.csv";
-    $csvRemoveFile = "./bulkFilesRemove.csv";
-    $outstream = fopen($csvFile, "w");
-    $outstreamRemove = fopen($csvRemoveFile, "w");
+    $matches = array();
     foreach ($bulks as &$row) {
-        $hash = hash("sha256", $row["text"]);
-        $file = "$bulkDir/$hash";
-        if (! file_exists($file)) {
-            file_put_contents($file, $row["text"]);
-        }
+      if(sizeof($row['addedLicenses']) > 0) {
+        $matches[] = [
+          'source' => 'BULK',
+          'text' => $row["text"],
+          'licenses' => $row['addedLicenses']
+        ];
+      }
 
-        foreach ($row['addedLicenses'] as &$addedLicense) {
-            $matchData = array();
-            $matchData[] = $file;
-            $matchData[] = $addedLicense;
-            $matchData[] = '';
-            $matchData[] = 'BULK';
-            $matchData[] = "";
-            $matchData[] = "";
-            __outputCSV($matchData, array(), $outstream);
-        }
-
-        foreach ($row['removedLicenses'] as &$removedLicense) {
-            $matchData = array();
-            $matchData[] = $file;
-            $matchData[] = $removedLicense;
-            $matchData[] = '';
-            $matchData[] = 'BULK_REMOVE';
-            $matchData[] = "";
-            $matchData[] = "";
-            __outputCSV($matchData, array(), $outstreamRemove);
-        }
-        echo "\n";
+      if(sizeof($row['removedLicenses']) > 0) {
+        $matches[] = [
+          'source' => 'BULK_REMOVE',
+          'text' => $row["text"],
+          'licenses' => $row['removedLicenses']
+        ];
+      }
     }
-    fclose($outstream);
-    fclose($outstreamRemove);
+
+    writeDataToMongo($matches, $mongoManager);
 }
 
-function dumpLicenseData()
+function dumpLicenseData($mongoManager)
 {
     /* @var $dbManager DbManager */
     $dbManager = $GLOBALS['container']->get('db.manager');
@@ -391,46 +284,50 @@ function dumpLicenseData()
     $dbManager->prepare($statementName, $sql);
     $result = $dbManager->execute($statementName, array());
 
-
-    $licensesDir="./licenses";
-    $csvFile = "./licenses.csv";
-    $outstream = fopen($csvFile, "w");
-    mkdir($licensesDir);
+    $matches = array();
     while ($row = $dbManager->fetchArray($result))
     {
-        $hash = hash("sha256", $row["rf_text"]);
         if ($row["rf_shortname_parent"] != null)
         {
-            $file = $licensesDir."/".str_replace("/", "_", $row["rf_shortname_parent"]."_".$row["rf_shortname"]."_".$hash);
-            $matchData = array();
-            $matchData[] = $file;
-            $matchData[] = $row["rf_shortname_parent"];
-            $matchData[] = '';
-            $matchData[] = 'LICENSE_INDIRECT_'.$row["usage"];
-            $matchData[] = "";
-            $matchData[] = "rf_pk=[".$row["rf_pk"]."]";
-            __outputCSV($matchData, array(), $outstream);
+          $matches[] = [
+            'source' => 'LICENSE_INDIRECT_'.$row["usage"],
+            'text' => $row["rf_text"],
+            'licenses' => [$row["rf_shortname_parent"]]
+          ];
         }
         else
         {
-            $file = $licensesDir."/".str_replace("/", "_", $row["rf_shortname"]."_".$hash);
-            $matchData = array();
-            $matchData[] = $file;
-            $matchData[] = $row["rf_shortname"];
-            $matchData[] = '';
-            $matchData[] = 'LICENSE';
-            $matchData[] = "";
-            $matchData[] = "rf_pk=[".$row["rf_pk"]."]";
-            __outputCSV($matchData, array(), $outstream);
-        }
-        if (! file_exists($file)) {
-            file_put_contents($file, $row["rf_text"]);
+          $matches[] = [
+            'source' => 'LICENSE',
+            'text' => $row["rf_text"],
+            'licenses' => [$row["rf_shortname"]]
+          ];
         }
     }
     $dbManager->freeResult($result);
+
+    writeDataToMongo($matches, $mongoManager);
 }
 
-function handleUpload($uploadtree_pk, $upload_pk, $user)
+function getItemTreeBounds($uploadtree_pk, $upload_pk)
+{
+    /* @var $uploadDao UploadDao */
+    $uploadDao = $GLOBALS['container']->get("dao.upload");
+
+    $uploadtreeTablename = GetUploadtreeTableName($upload_pk);
+    /** @var ItemTreeBounds */
+    return $uploadDao->getItemTreeBounds($uploadtree_pk, $uploadtreeTablename);
+}
+
+function getLicenseList($uploadtree_pk, $upload_pk, DbManager $dbManager)
+{
+  /** @var ItemTreeBounds */
+  $itemTreeBounds = getItemTreeBounds($uploadtree_pk, $upload_pk);
+
+  return getClearedLicensesForDump($dbManager, $itemTreeBounds);
+}
+
+function handleUpload($uploadtree_pk, $upload_pk, $user, $mongoManager)
 {
     $return_value = read_permission($upload_pk, $user); // check if the user has the permission to read this upload
     if (empty($return_value))
@@ -447,7 +344,6 @@ function handleUpload($uploadtree_pk, $upload_pk, $user)
             array($upload_pk),
             __METHOD__.'.find.uploadtree.to.use.in.browse.link' );
         $uploadtree_pk = $uploadtreeRec['uploadtree_pk'];
-        error_log("... determined uploadtree_pk=[$uploadtree_pk])");
     }
 
     if (empty($uploadtree_pk)) {
@@ -455,10 +351,12 @@ function handleUpload($uploadtree_pk, $upload_pk, $user)
         return;
     }
 
-    getLicenseList($uploadtree_pk, $upload_pk, $dbManager);
+
+    $clearedLicenses = getLicenseList($uploadtree_pk, $upload_pk, $dbManager);
+    writeDataToMongo($clearedLicenses, $mongoManager);
 }
 
-function handleAllUploads($user)
+function handleAllUploads($user, $mongoManager)
 {
     /* @var $dbManager DbManager */
     $dbManager = $GLOBALS['container']->get('db.manager');
@@ -467,7 +365,7 @@ function handleAllUploads($user)
 
     $dbManager->prepare("getAllUploads", $sql);
 
-
+    touch("already_done_upload_tree_pks");
     $alreadyDoneString = file_get_contents("already_done_upload_tree_pks");
 
     $result = $dbManager->execute("getAllUploads", array());
@@ -483,7 +381,7 @@ function handleAllUploads($user)
 
             error_log("Handle upload_pk=[$upload_pk] (uploadtree_pk=[])");
             try {
-                handleUpload('', $upload_pk, $user);
+                handleUpload('', $upload_pk, $user, $mongoManager);
                 file_put_contents("already_done_upload_tree_pks", "[$upload_pk]", FILE_APPEND | LOCK_EX);
             }catch ( Exception $e ) {
                 error_log("... failed to handle upload_pk=[$upload_pk]");
@@ -498,16 +396,18 @@ if (is_numeric($item) && !is_numeric($upload)) $upload = GetUploadID($item);
 
 account_check($user, $passwd); // check username/password
 
-/** check if parameters are valid */
+$mongourl = "mongodb://${mongouser}:${mongopasswd}@${mongohost}/rigel";
+$mongoManager = new MongoDB\Driver\Manager($mongourl);
+
 if (!is_numeric($upload) || (!empty($item) && !is_numeric($item)))
 {
-    handleAllUploads($user);
-    dumpBulkList("uploadtree_a");
-    dumpLicenseData();
+  handleAllUploads($user, $mongoManager);
+  dumpBulkList("uploadtree_a", $mongoManager);
+  dumpLicenseData($mongoManager);
 }
 else
 {
-    handleUpload($item, $upload, $user);
+  handleUpload($item, $upload, $user, $mongoManager);
 }
 
 return 0;
