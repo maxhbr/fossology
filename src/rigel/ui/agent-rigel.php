7769e3d4d94940b09fee63f4ccf81953a366b8d0
Maximilian Huber <maximilian.huber@tngtech.com>
2018-11-29 16:10:39 +0100

    fix(rigel): supress output of curl

--- src/rigel/ui/agent-rigel.php
+++ src/rigel/ui/agent-rigel.php
@@ -44,7 +44,7 @@ class RigelAgentPlugin extends AgentPlugin
 
   public function isRigelInstalled()
   {
-    exec('curl -G localhost:82/model', $lines, $returnVar);
+    exec('curl -s -G localhost:82/model', $lines, $returnVar);
     return (0==$returnVar);
   }
 }
